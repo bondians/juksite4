bd7c10fd3afa8f82dd74823092d3170fc454abad
David Henderson <cayuse@deepbondi.net>
2015-06-20 12:44:02 -0700

    added play history

--- app/controllers/currentsongs_controller.rb
+++ app/controllers/currentsongs_controller.rb
@@ -1,6 +1,13 @@
 class CurrentsongsController < ApplicationController
-    def index
+
+  def index
     params[:page] ||= 1
-    @playentries = Currentsong.paginate :page => params[:page], :order => 'id DESC'
+    @playentries = Currentsong.paginate(page: params[:page], :per_page => 50).order('id DESC')    
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @songs }
+    end
   end
+
 end
+
