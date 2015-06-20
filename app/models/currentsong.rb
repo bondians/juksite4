a33366f2b867cd30d6ae61c0d6409e8911d92161
David Henderson <cayuse@deepbondi.net>
2015-06-20 12:11:43 -0700

    stuff

--- app/models/currentsong.rb
+++ app/models/currentsong.rb
@@ -2,10 +2,8 @@ class Currentsong < ActiveRecord::Base
   belongs_to :song
   
   def self.playing
-# to do put this back for osx
-song = "none"
-    #song = `defaults read com.deepbondi.cocoaJukebox kCurrentSong`
-    #song.chomp!
+    song = `defaults read com.deepbondi.cocoaJukebox kCurrentSong`
+    song.chomp!
   end
   
   def self.setPlaying (song)
